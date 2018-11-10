676ca0a92e006c72d29170f3bcbc6d43977d50fe
Justin Livi <silaslivi@gmail.com>
2018-11-10 15:35:43 -0800

    Add new lines

--- frontend/src/components/CreateForm.tsx
+++ frontend/src/components/CreateForm.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Button, Col, ControlLabel, Form, FormControl, FormGroup } from 'react-bootstrap';
 
-export const enum CreateFormFields {
+export enum CreateFormFields {
     AdName = 'AdName',
     ConversionName = 'ConversionName',
     ConversionType = 'ConversionType',
@@ -12,34 +12,61 @@ export interface CreateFormProps {
     onSubmit: () => void;
 }
 
-export const CreateForm: React.SFC<CreateFormProps> = ({ onSubmit }) => (
+export const CreateForm: React.SFC<CreateFormProps> = ({ onSubmit, onChange }) => (
     <Form horizontal={true}>
         <FormGroup>
-            <Col componentClass={ControlLabel} sm={2}>
+            <Col
+                componentClass={ControlLabel}
+                sm={2}
+            >
                 Ad Name
             </Col>
             <Col sm={10}>
-                <FormControl type="text"/>
+                <FormControl
+                    type="text"
+                    onChange={(e) => {
+                        onChange(CreateFormFields.AdName, (e.target as HTMLInputElement).value)
+                    }}
+                />
             </Col>
         </FormGroup>
         <FormGroup>
-            <Col componentClass={ControlLabel} sm={2}>
+            <Col
+                componentClass={ControlLabel}
+                sm={2}
+            >
                 Conversion Name
             </Col>
             <Col sm={10}>
-                <FormControl type="text"/>
+                <FormControl
+                    type="text"
+                    onChange={(e) => {
+                        onChange(CreateFormFields.ConversionName, (e.target as HTMLInputElement).value)
+                    }}
+                />
             </Col>
         </FormGroup>
         <FormGroup>
-            <Col componentClass={ControlLabel} sm={2}>
+            <Col
+                componentClass={ControlLabel}
+                sm={2}
+            >
                 Conversion Type
             </Col>
             <Col sm={10}>
-                <FormControl type="text"/>
+                <FormControl
+                    type="text"
+                    onChange={(e) => {
+                        onChange(CreateFormFields.ConversionType, (e.target as HTMLInputElement).value)
+                    }}
+                />
             </Col>
         </FormGroup>
         <FormGroup>
-            <Col sm={10} smOffset={2} >
+            <Col
+                sm={10}
+                smOffset={2}
+            >
                 <Button onSubmit={onSubmit} >
                     Create
                 </Button>
