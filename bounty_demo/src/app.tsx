039da040dad16e8d84fd79e081c5837da4111226
Justin Livi <silaslivi@gmail.com>
2018-11-11 10:36:33 -0800

    Add basic crowd source UI

--- bounty_demo/src/app.tsx
+++ bounty_demo/src/app.tsx
@@ -1,10 +1,83 @@
-import './styles/App.css';
-
 import * as React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Col, Grid, Row } from 'react-bootstrap';
 
-export const App = () => (
-  <div>
-    <Button>Click Me!</Button>
-  </div>
-);
+export enum CompleteTaskStates {
+  Unsubmitted,
+  Submitting,
+  Submitted
+}
+
+export interface CompleteTaskState {
+  completed: boolean;
+  state: CompleteTaskStates;
+}
+
+export class App extends React.Component<{}, CompleteTaskState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      completed: false,
+      state: CompleteTaskStates.Unsubmitted
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  async handleSubmit() {
+    this.setState(prev => ({
+      ...prev,
+      state: CompleteTaskStates.Submitting
+    }));
+    try {
+      // hardcoded for simplicity
+      await fetch('http://localhost:3002/useraaaaaaaa/0', {
+        method: 'post'
+      });
+      this.setState({
+        state: CompleteTaskStates.Submitted,
+        completed: true
+      });
+    } catch {
+      this.setState(prev => ({
+        ...prev,
+        state: CompleteTaskStates.Unsubmitted
+      }));
+    }
+  }
+
+  render() {
+    const { completed, state } = this.state;
+    return (
+      <Grid>
+        <Row>
+          <Col sm={12}>
+            <Row>
+              <h1>Crowd Sourcing FTW</h1>
+            </Row>
+            <Row>
+              <h2>The best crowd sourcing platform</h2>
+            </Row>
+            <Row>
+              <h3>
+                Crowd sourcing is great, especially when you can verify work
+                completed!
+              </h3>
+            </Row>
+            <Row>
+              <br />
+              {completed ? (
+                <div>Task Complete!</div>
+              ) : (
+                <Button
+                  onClick={this.handleSubmit}
+                  disabled={state === CompleteTaskStates.Submitting}
+                >
+                  Complete my crowd sourced task
+                </Button>
+              )}
+            </Row>
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
+}
