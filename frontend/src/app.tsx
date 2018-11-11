136ceb32eaf5be0a17f36c905229f467400a03db
Justin Livi <silaslivi@gmail.com>
2018-11-10 19:48:19 -0800

    Resolve validation bug

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -9,13 +9,41 @@ import { Active } from './pages/Active';
 import { Completed } from './pages/Completed';
 import { CreateCampaign } from './pages/CreateCampaign';
 
+export interface Campaign {
+  unique_id: number;
+  target_users: number;
+  activated_users: number;
+  user_reward: {
+    symbol: string;
+    amount: number;
+  };
+}
+
+export interface DataType {
+  active_campaigns: Campaign[];
+}
+
+export const defaultData: DataType = {
+  active_campaigns: [
+    {
+      unique_id: 100,
+      target_users: 100,
+      activated_users: 100,
+      user_reward: {
+        symbol: 'something',
+        amount: 1,
+      },
+    },
+  ],
+};
+
 export const App: React.SFC<{}> = () => (
   <Router>
     <React.Fragment>
       <div className="nav-shadow" />
       <TopNav />
       <Store
-        defaultData={{}}
+        defaultData={defaultData}
         render={({ data }) => (
           <React.Fragment>
             <Route path="/active/" component={() => <Active data={data} />} />
