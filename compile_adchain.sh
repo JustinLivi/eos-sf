67d12ab913c7e748265ebac8fb1426b2721558dd
gamedevsam <sambatista1@gmail.com>
2018-11-11 01:03:28 -0500

    Removing references to notechain

--- compile_adchain.sh
+++ compile_adchain.sh
@@ -2,4 +2,4 @@ mkdir -p ./eosio_docker/compiled_contracts
 mkdir -p ./eosio_docker/compiled_contracts/adchain
 eosio-cpp -abigen "./eosio_docker/contracts/adchain/adchain.cpp" -o "./eosio_docker/compiled_contracts/adchain/adchain.wasm" --contract "adchain"
 
-docker exec eosio_notechain_container cleos set contract eosio compiled_contracts/adchain
+docker exec eosio cleos set contract adchainacc compiled_contracts/adchain
