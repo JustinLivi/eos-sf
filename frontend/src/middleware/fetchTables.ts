e565cf242263a094090877dd3b577f6676517095
gamedevsam <sambatista1@gmail.com>
2018-11-11 10:11:50 -0800

    Fix acounts

--- frontend/src/middleware/fetchTables.ts
+++ frontend/src/middleware/fetchTables.ts
@@ -4,7 +4,7 @@ import { Middleware } from 'redux';
 
 import { ActionTypes } from '../actions';
 
-const owner = 'adchainacc';
+const owner = 'pactacc';
 const endpoint = 'http://localhost:8888';
 
 export const fetchTableMiddleware: Middleware = store => next => action => {
@@ -15,7 +15,7 @@ export const fetchTableMiddleware: Middleware = store => next => action => {
         json: true,
         code: owner, // contract who owns the table
         scope: owner, // scope of the table
-        table: 'creators', // name of the table as specified by the contract abi
+        table: 'users', // name of the table as specified by the contract abi
         limit: 100
       })
       .then(result => {
